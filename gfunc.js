00cca0237a23894143da90e3e03091b021a76113
Grisha Trubetskoy <grisha@grisha.org>
2017-03-17 18:25:33 -0400

    Comment out map/reduceSeries

--- gfunc.js
+++ gfunc.js
@@ -33,6 +33,13 @@ function (_, $) {
         { name: 'other', type: 'value_or_series', optional: true }
     ];
 
+    // addFuncDef({
+    //     name: 'generate',
+    //     category: categories.Transform,
+    //     params: [],
+    //     defaultParams: [],
+    // });
+
     addFuncDef({
         name: 'scaleToSeconds',
         category: categories.Transform,
@@ -115,26 +122,26 @@ function (_, $) {
         category: categories.Combine,
     });
 
-    addFuncDef({
-        name: 'mapSeries',
-        shortName: 'map',
-        params: [{ name: "node", type: 'int' }],
-        defaultParams: [3],
-        category: categories.Combine,
-    });
+    // addFuncDef({
+    //     name: 'mapSeries',
+    //     shortName: 'map',
+    //     params: [{ name: "node", type: 'int' }],
+    //     defaultParams: [3],
+    //     category: categories.Combine,
+    // });
 
-    addFuncDef({
-        name: 'reduceSeries',
-        shortName: 'reduce',
-        params: [
-            { name: "function", type: 'string', options: ['asPercent', 'diffSeries', 'divideSeries'] },
-            { name: "reduceNode", type: 'int', options: [0,1,2,3,4,5,6,7,8,9,10,11,12,13] },
-            { name: "reduceMatchers", type: 'string' },
-            { name: "reduceMatchers", type: 'string' },
-        ],
-        defaultParams: ['asPercent', 2, 'used_bytes', 'total_bytes'],
-        category: categories.Combine,
-    });
+    // addFuncDef({
+    //     name: 'reduceSeries',
+    //     shortName: 'reduce',
+    //     params: [
+    //         { name: "function", type: 'string', options: ['asPercent', 'diffSeries', 'divideSeries'] },
+    //         { name: "reduceNode", type: 'int', options: [0,1,2,3,4,5,6,7,8,9,10,11,12,13] },
+    //         { name: "reduceMatchers", type: 'string' },
+    //         { name: "reduceMatchers", type: 'string' },
+    //     ],
+    //     defaultParams: ['asPercent', 2, 'used_bytes', 'total_bytes'],
+    //     category: categories.Combine,
+    // });
 
     addFuncDef({
         name: 'sumSeries',
